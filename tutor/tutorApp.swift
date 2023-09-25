ca9035ad8d30ecf62db45d598aa57d4502538d6e
JOB <eyupensarorl@gmail.com>
2023-09-25 17:14:16 +0300

    Initial Commit

---
+++ tutor/tutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorApp.swift
+//  tutor
+//
+//  Created by eyupeo on 16.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct tutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
